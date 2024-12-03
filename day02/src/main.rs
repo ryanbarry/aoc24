2ca8fa0b1ce1a869c898fd6b472cd68ff3d4225a
Ryan Barry <ryan@nuclearice.com>
2024-12-03 01:09:22 -0800

    do the solution for part1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,29 +8,38 @@ const TRIVIAL_EXAMPLE: &str = r#"7 6 4 2 1
 fn main() {
     let num_safe = count_safe_lines(TRIVIAL_EXAMPLE.split_terminator('\n'));
     println!("[trivial] number of safe reports: {}", num_safe);
+
+    if let Ok(part1_reports) = std::fs::read_to_string("input.txt") {
+        let num_safe = count_safe_lines(part1_reports.split_terminator('\n'));
+        println!("[partone] number of safe reports: {}", num_safe);
+    } else {
+        println!("could not read input file");
+    }
 }
 
 fn count_safe_lines<'a>(lines: impl Iterator<Item = &'a str>) -> usize {
-    lines.map(|line| check_report_safety(line)).filter(|x| *x).count()
+    lines
+        .map(|line| check_report_safety(line))
+        .filter(|x| *x)
+        .count()
 }
 
 fn check_report_safety(line: &str) -> bool {
-    let levels = line.split_ascii_whitespace().map(|s| s.parse().expect("couldn't parse number")).collect::<Vec<usize>>();
+    let levels = line
+        .split_ascii_whitespace()
+        .map(|s| s.parse().expect("couldn't parse number"))
+        .collect::<Vec<usize>>();
 
     let a = levels[0];
-    let b = levels[0+1];
+    let b = levels[0 + 1];
     let diff = a.abs_diff(b);
     if diff < 1 || diff > 3 {
         return false;
     }
-    let decreasing = if a > b {
-        true
-    } else {
-        false
-    };
-    for i in 1..levels.len()-1 {
+    let decreasing = if a > b { true } else { false };
+    for i in 1..levels.len() - 1 {
         let a = levels[i];
-        let b = levels[i+1];
+        let b = levels[i + 1];
         let diff = a.abs_diff(b);
         if diff < 1 || diff > 3 {
             return false;
