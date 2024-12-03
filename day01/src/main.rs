bdc97c6dd1e9b69880c44c05f4ec50c89425f76c
Ryan Barry <ryan@nuclearice.com>
2024-12-02 19:28:37 -0800

    sorting

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,7 +7,21 @@ const TRIVIAL_EXAMPLE: &str = r#"3   4
 
 fn main() {
     let (list1, list2) = collect_lists(TRIVIAL_EXAMPLE);
-    list1.iter().zip(list2).for_each(|(x, y)| {
+    list1.iter().zip(list2.clone()).for_each(|(x, y)| {
+        println!("{} {}", x, y);
+    });
+    println!("now, for my next trick: sorting!");
+    let list1 = {
+        let mut tmp = list1.clone();
+        tmp.sort();
+        tmp
+    };
+    let list2 = {
+        let mut tmp = list2.clone();
+        tmp.sort();
+        tmp
+    };
+    list1.iter().zip(list2.clone()).for_each(|(x, y)| {
         println!("{} {}", x, y);
     });
 }
@@ -18,11 +32,15 @@ fn collect_lists(input: &str) -> (Vec<isize>, Vec<isize>) {
     let mut list2 = vec![];
 
     for (idx, line) in input.split_terminator('\n').enumerate() {
-        let lineno = idx+1;
+        let lineno = idx + 1;
         let strings: Vec<_> = line.split_ascii_whitespace().collect();
         assert_eq!(strings.len(), 2, "line {} doesn't have 2 numbers", lineno);
-        let num1: isize = strings[0].parse().expect(format!("first number on line {} doesn't parse", lineno).as_str());
-        let num2: isize = strings[1].parse().expect(format!("second number on line {} doesn't parse", lineno).as_str());
+        let num1: isize = strings[0]
+            .parse()
+            .expect(format!("first number on line {} doesn't parse", lineno).as_str());
+        let num2: isize = strings[1]
+            .parse()
+            .expect(format!("second number on line {} doesn't parse", lineno).as_str());
         list1.push(num1);
         list2.push(num2);
     }
