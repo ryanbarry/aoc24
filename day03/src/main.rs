b7dfca5cdbf3e089f50b40dbd1d50fff0c9d3c39
Ryan Barry <ryan@nuclearice.com>
2024-12-05 18:55:12 -0800

    day03 part2 done

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,8 @@ use regex_lite::Regex;
 
 const TRIVIAL_EXAMPLE: &str =
     "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+const TRIVIAL_EXAMPLE2: &str =
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
 fn main() {
     let total = parse_and_exec(TRIVIAL_EXAMPLE);
@@ -14,6 +16,11 @@ fn main() {
 
     let total = parse_and_exec(&input);
     println!("[partone] {}", total);
+
+    let total = parse_and_exec_p2(TRIVIAL_EXAMPLE2);
+    println!("[2rivial] {}", total);
+    let total = parse_and_exec_p2(&input);
+    println!("[parttwo] {}", total);
 }
 
 #[test]
@@ -23,7 +30,8 @@ fn test_trivial() {
 }
 
 fn parse_and_exec(input: &str) -> usize {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("couldn't compile regex");
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")
+        .expect("couldn't compile regex");
 
     re.captures_iter(input)
         .map(|m| m.extract().1)
@@ -36,6 +44,54 @@ fn parse_and_exec(input: &str) -> usize {
                 .parse()
                 .expect(&format!("failed to parse first arg of match {}", idx));
             x * y
+        })
+        .sum()
+}
+
+fn parse_and_exec_p2(input: &str) -> usize {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do(\()(\))|don't(\()(\))")
+        .expect("couldn't compile regex");
+    let mut enabled = true;
+    enum EnSignal {
+        Enabled,
+        Disabled,
+        Unchanged,
+    }
+
+    re.captures_iter(input)
+        .map(|m| {
+            let (fullstr, capts) = m.extract();
+            if fullstr.starts_with("don't") {
+                (EnSignal::Disabled, capts)
+            } else if fullstr.starts_with("do") {
+                (EnSignal::Enabled, capts)
+            } else {
+                (EnSignal::Unchanged, capts)
+            }
+        })
+        .enumerate()
+        .filter_map(|(idx, (newen, [a, b]))| match newen {
+            EnSignal::Enabled => {
+                enabled = true;
+                None
+            }
+            EnSignal::Disabled => {
+                enabled = false;
+                None
+            }
+            EnSignal::Unchanged => {
+                let x: usize = a
+                    .parse()
+                    .expect(&format!("failed to parse first arg of match {}", idx));
+                let y: usize = b
+                    .parse()
+                    .expect(&format!("failed to parse first arg of match {}", idx));
+                if enabled {
+                    Some(x * y)
+                } else {
+                    Some(0)
+                }
+            }
         })
         .sum()
 }
